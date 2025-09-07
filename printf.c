0b096af5a8b5f1c7cffeeb38799cb4b13cb7bc2c
Rahul-Git-Projects <rahul.sanapala2@gmail.com>
2025-09-07 14:26:14 +0530

    did some problems with my current knowledge of c

---
+++ printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() 
+{
+	
+    char st[100];
+    printf("Hello, World!");
+    scanf("%[^\n]",st);
+    printf("\n%s",st);
+    
+  	
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    return 0;
+}
